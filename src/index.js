4c5b6aad121debc425c1685731de8bf81f01c7a9
Kamran Ahmed <kamranahmed.se@gmail.com>
2018-10-27 22:38:26 +0400

    Dmg generation and bundle improvements

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'nprogress/nprogress.css';
 
 import './global.css';
-import * as serviceWorker from './worker';
 import Browser from './browser';
 
 ReactDOM.render(<Browser/>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
