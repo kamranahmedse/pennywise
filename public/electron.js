6605ac6ca9a98c6074f560c89bdb7532e741cc7b
Akash <jakash1997@gmail.com>
2018-11-10 08:33:28 +0530

    yeet

---
+++ public/electron.js
@@ -0,0 +1,116 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
+const pdfWindow = require('electron-pdf-window');
+const path = require('path');
+
+const { setMainMenu } = require('./menu');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    title: 'Pennywise',
+    width: 700,
+    height: 600,
+    autoHideMenuBar: true,
+    backgroundColor: '#16171a',
+    show: false,
+    webPreferences: {
+      plugins: true
+    },
+  });
+
+  pdfWindow.addSupport(mainWindow);
+
+  const isDev = !!process.env.APP_URL;
+  if (process.env.APP_URL) {
+    mainWindow.loadURL(process.env.APP_URL);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../build/index.html'));
+  }
+
+  // Show the window once the content has been loaded
+  mainWindow.on('ready-to-show', () => {
+    // Hide the dock icon before showing and
+    // show it once the app has been displayed
+    // @link https://github.com/electron/electron/issues/10078
+    // @fixme hack to make it show on full-screen windows
+    app.dock && app.dock.hide();
+    mainWindow.show();
+    app.dock && app.dock.show();
+
+    // Set the window to be always on top
+    mainWindow.setAlwaysOnTop(true);
+    mainWindow.setVisibleOnAllWorkspaces(true);
+    mainWindow.setFullScreenable(false);
+
+    bindIpc();
+  });
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+
+  // Open the dev tools only for dev
+  // and when the flag is not set
+  if (isDev && !process.env.DEV_TOOLS) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  setMainMenu(mainWindow);
+}
+
+// Binds the methods for renderer/electron communication
+function bindIpc() {
+  // Binds the opacity getter functionality
+  ipcMain.on('opacity.get', (event) => {
+    // Multiplying by 100 – browser range is 0 to 100
+    event.returnValue = mainWindow.getOpacity() * 100;
+  });
+
+  ipcMain.on('opacity.set', (event, opacity) => {
+    // Divide by 100 – window range is 0.1 to 1.0
+    mainWindow.setOpacity(opacity / 100);
+  });
+}
+
+// Makes the app start receiving the mouse interactions again
+function disableDetachedMode() {
+  app.dock && app.dock.setBadge('');
+  mainWindow && mainWindow.setIgnoreMouseEvents(false);
+}
+
+function checkAndDownloadUpdate() {
+  try {
+    autoUpdater.checkForUpdatesAndNotify();
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', function () {
+  createWindow();
+  checkAndDownloadUpdate();
+});
+
+// Make the window start receiving mouse events on focus/activate
+app.on('browser-window-focus', disableDetachedMode);
+app.on('activate', disableDetachedMode);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  app.quit();
+});
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ public/menu.js
@@ -0,0 +1,195 @@
+const { app, dialog, Menu, shell } = require('electron');
+
+module.exports = {
+  setMainMenu
+};
+
+const isWindows = process.platform === 'win32';
+const isMac = process.platform === 'darwin';
+const appVersion = app.getVersion();
+
+/**
+ * Gets the updated opacity with the given update factor
+ * @param currentOpacity
+ * @param updateFactor
+ * @return {*}
+ */
+function getUpdatedOpacity(currentOpacity, updateFactor) {
+  let newOpacity = currentOpacity + updateFactor;
+
+  if (newOpacity >= 1) {
+    return 1;
+  } else if (newOpacity <= 0.2) {
+    return 0.2;
+  } else {
+    return newOpacity;
+  }
+}
+
+/**
+ * Conditionally gets the menu items for changing window
+ * opacity if it is supported
+ * @param mainWindow
+ * @return {array}
+ */
+function getOpacityMenuItems(mainWindow) {
+  if (!isWindows && !isMac) {
+    return [];
+  }
+
+  return [
+    {
+      label: 'Decrease Opacity',
+      accelerator: 'CmdOrCtrl+Shift+Down',
+      click() {
+        const nextOpacity = getUpdatedOpacity(mainWindow.getOpacity(), -0.1);
+
+        mainWindow.webContents.send('opacity.sync', nextOpacity * 100);
+        mainWindow.setOpacity(nextOpacity);
+      }
+    },
+    {
+      label: 'Increase Opacity',
+      accelerator: 'CmdOrCtrl+Shift+Up',
+      click() {
+        const nextOpacity = getUpdatedOpacity(mainWindow.getOpacity(), 0.1);
+
+        mainWindow.webContents.send('opacity.sync', nextOpacity * 100);
+        mainWindow.setOpacity(nextOpacity);
+      }
+    },
+    { type: 'separator' },
+  ];
+}
+
+/**
+ * Sets the main menu
+ * @param mainWindow
+ */
+function setMainMenu(mainWindow) {
+  const template = [
+    {
+      label: isWindows ? 'File' : app.getName(),
+      submenu: [
+        {
+          label: 'Open',
+          accelerator: 'CmdOrCtrl+O',
+          click() {
+            dialog.showOpenDialog(function (fileNames) {
+              if (!fileNames || !fileNames[0]) {
+                return;
+              }
+
+              // For the file URLs, load them directly
+              // Append the `file://` prefix otherwise
+              const url = /^file:\/\/\//.test(fileNames[0]) ? fileNames[0] : `file://${fileNames[0]}`;
+              mainWindow.loadURL(url);
+            });
+          }
+        },
+        { role: 'close' },
+        { type: 'separator' },
+        { role: 'quit' },
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Embed Videos',
+          type: 'checkbox',
+          checked: true,
+          click(menuItem) {
+            mainWindow.webContents.send('embedVideos.set', menuItem.checked);
+          }
+        },
+        { type: 'separator' },
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'selectall' }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        ...getOpacityMenuItems(mainWindow),
+        {
+          label: 'Toggle Navbar',
+          accelerator: 'CmdOrCtrl+Shift+L',
+          click() {
+            mainWindow.webContents.send('nav.toggle');
+          }
+        },
+        {
+          label: 'Focus URL',
+          accelerator: 'CmdOrCtrl+L',
+          click() {
+            mainWindow.webContents.send('nav.show');
+            mainWindow.webContents.send('nav.focus');
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Detached Mode',
+          accelerator: 'CmdOrCtrl+Shift+D',
+          click() {
+            app.dock && app.dock.setBadge('Detached');
+            mainWindow.setIgnoreMouseEvents(true);
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Developer Tools',
+          accelerator: 'CmdOrCtrl+Alt+I',
+          click() {
+            mainWindow.webContents.openDevTools();
+          }
+        },
+        { type: 'separator' },
+        { role: 'resetzoom' },
+        { role: 'zoomin', accelerator: 'CmdOrCtrl+=' },
+        { role: 'zoomout' },
+        { type: 'separator' },
+      ]
+    },
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'Found a Bug',
+          click() {
+            shell.openExternal('https://github.com/kamranahmedse/pennywise/issues/new');
+          }
+        },
+        {
+          label: 'Suggestions',
+          click() {
+            shell.openExternal('https://github.com/kamranahmedse/pennywise/issues/new');
+          }
+        },
+        {
+          label: 'Learn More',
+          click() {
+            shell.openExternal('https://github.com/kamranahmedse');
+          }
+        },
+        {
+          label: `About Version`,
+          click() {
+            shell.openExternal(`https://github.com/kamranahmedse/pennywise/releases/tag/${appVersion}`);
+          }
+        },
+      ]
+    }
+  ];
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
